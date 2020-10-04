3631120d8ab3a13972287443bf1628e5e98c0d30
Michael Cheung <56772737+cheung0@users.noreply.github.com>
2020-10-03 19:33:56 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+from flask_socketio import join_room, leave_room
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'passwordispassword'
+socketio = SocketIO(app)
+
+
+@app.route('/')
+def sessions():
+    return render_template('index.html')
+
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    send(username + ' has entered the room.', room=room)
+
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', room=room)
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
